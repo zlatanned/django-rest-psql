b6e52326e9034d36b8a0232e5a1543f3ad47893d
zlatanned <shahiakshay31@gmail.com>
2021-07-01 13:29:35 +0530

    add cors middleware

--- DjangoRestPsql/settings.py
+++ DjangoRestPsql/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
