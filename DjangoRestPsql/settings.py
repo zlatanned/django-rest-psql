0d537fcde2e5588d56e26fd1c2ad23d0752027b3
zlatanned <shahiakshay31@gmail.com>
2021-07-01 13:12:13 +0530

    added database info in settings

--- DjangoRestPsql/settings.py
+++ DjangoRestPsql/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DjangoRestPsql.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
