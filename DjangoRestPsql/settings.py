e961a02521cfea7a5cc0335523ca078b483d8e72
zlatanned <shahiakshay31@gmail.com>
2021-07-01 13:32:27 +0530

    add cors origin whitelist in settings.py

--- DjangoRestPsql/settings.py
+++ DjangoRestPsql/settings.py
@@ -57,6 +57,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
+
 ROOT_URLCONF = 'DjangoRestPsql.urls'
 
 TEMPLATES = [
